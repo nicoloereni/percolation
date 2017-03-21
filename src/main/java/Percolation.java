f86bc97b518f9486590fd894560122579a464193
Nicolo Ereni <nicolo@Nicolos-MacBook-Pro.local>
2017-03-21 22:23:52 +0100

    dummy solution

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -2,22 +2,26 @@ package main.java;
 
 public class Percolation
 {
-    public static final int BLOCKED = 0;
+    private static final int BLOCKED = 0;
     private static final int OPEN = 1;
+    private static final int FULL = 2;
     private int dimension;
     private Integer [][] grid;
 
     // create n-by-n grid, with all sites blocked
     public Percolation(int n)
     {
+
         validateDimension(n);
         this.dimension = n;
         this.grid = new Integer[this.dimension][this.dimension];
 
         initGrid();
+
     }
 
     private void initGrid() {
+
         //TODO loop duplicated
         for(int rowIndex = 1; rowIndex <= dimension; rowIndex++)
         {
@@ -26,48 +30,142 @@ public class Percolation
                 assignGridValueAt(rowIndex, columnIndex, BLOCKED);
             }
         }
+
     }
 
     private void assignGridValueAt(int rowIndex, int columnIndex, int value) {
+
         grid[rowIndex -1][columnIndex -1] = value;
+
     }
 
     private void validateDimension(int dimension) throws IllegalArgumentException
     {
+
         if(dimension <= 0)
         {
             throw new IllegalArgumentException("Wrong dimension");
         }
+
     }
 
     // open site (row, col) if it is not open already
     public void open(int row, int col)
     {
+
         validateCoordinates(row, col);
         assignGridValueAt(row, col, OPEN);
+
     }
 
     // is site (row, col) open?
     public boolean isOpen(int row, int col)
     {
+
         validateCoordinates(row, col);
         return gridValueAt(row, col) == OPEN;
+
     }
 
     private Integer gridValueAt(int row, int column) {
+
         return grid[row -1][column -1];
+
     }
 
     // is site (row, col) full?
     public boolean isFull(int row, int col)
     {
+
         validateCoordinates(row, col);
-        return false;
+
+        boolean result = false;
+        boolean open = isOpen(row, col);
+
+        if(gridValueAt(row, col) == FULL)
+        {
+            result = true;
+        }
+        else if (row == 1 && open)
+        {
+            assignGridValueAt(row, col, FULL);
+            result = true;
+        }
+        else if(open && hasFullNeighboring(row, col))
+        {
+            assignGridValueAt(row, col, FULL);
+            result = true;
+        }
+
+        return result;
+
+    }
+
+    private boolean hasFullNeighboring(int row, int col) {
+
+        boolean result = false;
+
+        if(gridValueAt(row, col) == FULL)
+        {
+            result = true;
+        }
+        else if(isLeftFull(row, col))
+        {
+            assignGridValueAt(row, col, FULL);
+            result = true;
+        }
+        else if(isRightFull(row, col))
+        {
+            assignGridValueAt(row, col, FULL);
+            result = true;
+        }
+        else if(isBottomFull(row, col))
+        {
+            assignGridValueAt(row, col, FULL);
+            result = true;
+        }
+        else if(isUpFull(row, col))
+        {
+            assignGridValueAt(row, col, FULL);
+            result = true;
+        }
+
+        return result;
+
+    }
+
+    private boolean isUpFull(int row, int col) {
+
+        int rowUpIndex = row +1;
+        return rowUpIndex <= this.dimension && isFull(rowUpIndex, col);
+
+    }
+
+    private boolean isBottomFull(int row, int col) {
+
+        int bottomRowIndex = row -1;
+        return bottomRowIndex > 0 && isFull(bottomRowIndex, col);
+
+    }
+
+    private boolean isRightFull(int row, int col) {
+
+        int rightColumnIndex = row + 1;
+        return rightColumnIndex <= this.dimension && isFull(row, rightColumnIndex);
+
+    }
+
+    private boolean isLeftFull(int row, int col) {
+
+        int leftColumnIndex = col -1;
+        return leftColumnIndex > 0 && isFull(row, leftColumnIndex);
+
     }
 
     // number of open sites
     public int numberOfOpenSites()
     {
+
         int result = 0;
 
         for(int rowIndex = 1; rowIndex <= dimension; rowIndex++)
@@ -78,23 +176,37 @@ public class Percolation
                 }
             }
         }
+
         return result;
+
     }
 
     // does the system percolate?
     public boolean percolates()
     {
-        return false;
+
+        boolean result = false;
+
+        for(int columnIndex = 1; columnIndex <= dimension; columnIndex++)
+        {
+            if(isFull(dimension, columnIndex))
+            {
+                result = true;
+            }
+        }
+
+        return result;
+
     }
 
     // test client (optional)
     public static void main(String[] args)
     {
-
     }
 
     private void validateCoordinates(int row, int column) throws IllegalArgumentException
     {
+
         validateDimension(row);
         validateDimension(column);
 
@@ -102,6 +214,7 @@ public class Percolation
         {
             throw new IllegalArgumentException("Wrong coordinate dimension");
         }
+
     }
 
 }
--- src/test/PercolationTest.java
+++ src/test/PercolationTest.java
@@ -8,16 +8,19 @@ import static org.junit.Assert.assertFalse;
 
 public class PercolationTest {
 
-    public static final int DEFAULT_TEST_DIMENSION = 3;
+    private static final int DEFAULT_TEST_DIMENSION = 3;
     private Percolation percolation;
 
     @Before
     public void setUp() throws Exception {
+
         percolation = new Percolation(DEFAULT_TEST_DIMENSION);
+
     }
 
     @Test
     public void gridInitToBlocked() throws Exception {
+
         for(int i = 1; i <= DEFAULT_TEST_DIMENSION; i++)
         {
             for(int j = 1; j <= DEFAULT_TEST_DIMENSION; j++)
@@ -25,75 +28,100 @@ public class PercolationTest {
                 assertFalse(percolation.isOpen(i,j));
             }
         }
+
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void constructorThrowIAEIfDimensionEqualToZero() throws Exception {
+
         new Percolation(0);
+
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void constructorThrowIAEIfDimensionLessThenZero() throws Exception {
+
         new Percolation(-1);
+
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void isOpenThrowIAEWhenRowIsBiggerThenDimension() throws Exception
     {
+
         percolation.isOpen(4,3);
+
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void isOpenThrowIAEWhenColumnIsBiggerThenDimension() throws Exception
     {
+
         percolation.isOpen(3,4);
+
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void isOpenThrowIAEWhenColumnIsLessThenZero() throws Exception
     {
+
         percolation.isOpen(3,-3);
+
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void isOpenThrowIAEWhenRowIsEqualToZero() throws Exception
     {
+
         percolation.isOpen(0,3);
+
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void openThrowIAEWhenColumnIsBiggerThenDimension() throws Exception
     {
+
         percolation.open(3,4);
+
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void openThrowIAEWhenRowIsBiggerThenDimension() throws Exception
     {
+
         percolation.open(4,3);
+
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void isFullThrowIAEWhenRowIsBiggerThenDimension() throws Exception
     {
+
         percolation.isFull(4,3);
+
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void isFullThrowIAEWhenColumnIsBiggerThenDimension() throws Exception
     {
+
         percolation.isFull(4,3);
+
     }
 
     @Test
     public void openSiteContainsOpenValue() throws Exception {
+
         assertFalse(percolation.isOpen(1,1));
+
         percolation.open(1,1);
         assertTrue(percolation.isOpen(1,1));
+
     }
 
     @Test
     public void numberOfOpenSitesReturnProperResponse() throws Exception {
+
         assertEquals(0, percolation.numberOfOpenSites());
 
         percolation.open(1,2);
@@ -104,5 +132,44 @@ public class PercolationTest {
 
         percolation.open(3,2);
         assertEquals(3, percolation.numberOfOpenSites());
+
+    }
+
+    @Test
+    public void isFullReturnProperResponseOpenBottomSite() throws Exception {
+
+        assertFalse(percolation.isFull(1,1));
+
+        percolation.open(1,1);
+        assertTrue(percolation.isFull(1,1));
+
+        percolation.open(3,1);
+        assertFalse(percolation.isFull(3,1));
+
+        percolation.open(2, 1);
+        assertTrue(percolation.isFull(2,1));
+        assertTrue(percolation.isFull(3,1));
+    }
+
+    @Test
+    public void isFullReturnProperResponseOpenLeftSite() throws Exception {
+
+        assertFalse(percolation.isFull(1,1));
+
+        percolation.open(1,1);
+        assertTrue(percolation.isFull(1,1));
+
+        percolation.open(2,2);
+        assertFalse(percolation.isFull(2,2));
+
+        percolation.open(2, 1);
+        assertTrue(percolation.isFull(2,1));
+        assertTrue(percolation.isFull(2,2));
+    }
+
+    @Test
+    public void name() throws Exception {
+        
+
     }
 }
